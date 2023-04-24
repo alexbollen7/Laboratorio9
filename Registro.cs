d04b097eda23e067127ed6cf502ff92fea0fb460
alexbollen <alexbollen@umes.edu.gt>
2023-04-24 08:41:35 -0600

    Creacion de clases de Registo y Cursos

---
+++ Registro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laboratorio9
+{
+    public class Registro
+    {
+        string nombre;
+        List<Curso> cursos;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public List<Curso> Cursos { get => cursos; set => cursos = value; }
+
+        //Constructor de la clase
+        //Ejecuta codigo al crearse un objeto de esta clase
+
+        public Registro(){
+            //Crear la lista de cursos
+            cursos = new List<Curso>();
+        }
+    }
+}
